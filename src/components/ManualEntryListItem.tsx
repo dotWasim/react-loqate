49c61591badff748cf73513bb71a15dcc9082fe5
Wasim Alatrash <w-elatrash@hotmail.com>
2023-12-10 19:15:03 +0400

    Added ManualEntryListItemProps

--- src/components/ManualEntryListItem.tsx
+++ src/components/ManualEntryListItem.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const ManualEntryListItem = ({ onClick, text }) => {
+interface ManualEntryListItemProps {
+  onClick?: () => void;
+  text: string;
+}
+
+const ManualEntryListItem: React.FC<ManualEntryListItemProps> = ({
+  onClick,
+  text,
+}) => {
   return (
     <li onClick={onClick} style={{ cursor: 'pointer', padding: '10px' }}>
       {text}
